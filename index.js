5a1793c8491bfbc58223c05e752f43bb1618b292
Tien Tran <tientran2407.vn@gmail.com>
2019-02-20 23:28:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,12 @@ printString = (word) => {
     printString(word.substring(1, word.length))
   }
 }
+
+
+reverseString = (word) => {
+  console.log(word[word.length - 1])
+  
+  if (word.length > 1){
+    reverseString(word.substring(0, word.length - 2))
+  }
+}
